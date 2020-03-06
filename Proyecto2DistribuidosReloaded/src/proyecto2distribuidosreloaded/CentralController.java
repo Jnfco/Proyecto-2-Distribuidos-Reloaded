71ec652a314830a2642fc115cc5b9ff888f51182
Juanfco <peinrinegan18@gmail.com>
2020-03-06 00:03:53 -0300

    reportes se muestran bien

--- Proyecto2DistribuidosReloaded/src/proyecto2distribuidosreloaded/CentralController.java
+++ Proyecto2DistribuidosReloaded/src/proyecto2distribuidosreloaded/CentralController.java
@@ -52,22 +52,32 @@ public class CentralController implements Initializable {
     String password = "Distribuidos1234";
     private ObservableList<ObservableList> data1;
     private ObservableList<ObservableList> data2;
-    private ObservableList<ObservableList> data3;
+    private Connection c1;
+    private Connection c2;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        data1 = FXCollections.observableArrayList();
-        data2 = FXCollections.observableArrayList();
-        data3 = FXCollections.observableArrayList();
-        
-        //this.tablaReporte = new TableView(mostrarReporte(tablaReporte));
-        data3.add(mostrarReporte(tablaReporte));
-        data3.add(mostrarReporte2(tablaReporte));
-        this.tablaReporte= new TableView(data3);
-        // TODO
+        try {
+            //Connection c = db1Connection();
+            //Connection c2 = db2Connection();
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            c1 = DriverManager.getConnection(urlDB1, user, password);
+            c2 = DriverManager.getConnection(urlDB2, user, password);
+
+            data1 = FXCollections.observableArrayList();
+            data2 = FXCollections.observableArrayList();
+
+            //this.tablaReporte = new TableView(mostrarReporte(tablaReporte));
+            //this.tablaReporte.setItems(mostrarReporte2(tablaReporte));
+            this.tablaReporte = new TableView(mostrarReporte(tablaReporte,data1,data2));
+            
+            // TODO
+        } catch (SQLException ex) {
+            Logger.getLogger(CentralController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @FXML
@@ -75,68 +85,31 @@ public class CentralController implements Initializable {
         System.out.println("Cancelar");
     }
 
-    public Connection db1Connection() {
-
-        try {
-
-            Class.forName("org.postgresql.Driver");
-
-        } catch (ClassNotFoundException e) {
-            e.getMessage();
-        }
-
-        try {
-            connection = DriverManager.getConnection(urlDB1, user, password);
-            //JOptionPane.showMessageDialog(null, "Connected");
-        } catch (SQLException ex) {
-            Logger.getLogger(Proyecto2DistribuidosReloaded.class.getName()).log(Level.SEVERE, null, ex);
-            JOptionPane.showMessageDialog(null, "Failed To Connect");
-        }
-
-        return connection;
-    }
-     public Connection db2Connection() {
-
-        try {
-
-            Class.forName("org.postgresql.Driver");
-
-        } catch (ClassNotFoundException e) {
-            e.getMessage();
-        }
-
-        try {
-            connection = DriverManager.getConnection(urlDB2, user, password);
-            //JOptionPane.showMessageDialog(null, "Connected");
-        } catch (SQLException ex) {
-            Logger.getLogger(Proyecto2DistribuidosReloaded.class.getName()).log(Level.SEVERE, null, ex);
-            JOptionPane.showMessageDialog(null, "Failed To Connect");
-        }
-
-        return connection;
-    }
-
     @FXML
-    public void handleActualizar() {
+    public void handleActualizar() throws SQLException {
+
         //System.out.println("Actualizando");
         data1.removeAll(data1);
         data2.removeAll(data2);
-        this.tablaReporte = new TableView(mostrarReporte(tablaReporte));
-        this.tablaReporte.setItems(mostrarReporte2(tablaReporte));
         
+        
+        
+        
+        this.tablaReporte = new TableView(mostrarReporte(tablaReporte,data1,data2));
+        this.tablaReporte.refresh();
+
     }
 
+    public ObservableList<ObservableList> mostrarReporte(TableView tablaEst1,ObservableList<ObservableList> data1,ObservableList<ObservableList> data2) throws SQLException {
 
-    public ObservableList<ObservableList> mostrarReporte(TableView tablaEst1) {
-        
         try {
-            Connection c = db1Connection();
+
             String sql = "Select surtidor.idsurtidor,distribuidor.iddistribuidor,surtidor.tipocombustible ,round(Avg(venta.cantidadlitros),2 )as promediolitros ,round(Avg(venta.valorventa),2) as promedioventa\n"
                     + "  From  distribuidor, surtidor, venta\n"
                     + "  where distribuidor.iddistribuidor = surtidor.iddistribuidor and surtidor.idsurtidor = venta.idsurtidor \n"
                     + "  Group By surtidor.idsurtidor, distribuidor.iddistribuidor;";
 
-            ResultSet rs = c.createStatement().executeQuery(sql);
+            ResultSet rs = c1.createStatement().executeQuery(sql);
 
             for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
                 //We are using non property style for making dynamic table
@@ -167,25 +140,46 @@ public class CentralController implements Initializable {
 
             //FINALLY ADDED TO TableView
             tablaEst1.setItems(data1);
+            
+            
+            //BD2
+            
+            ResultSet rs2 = c2.createStatement().executeQuery(sql);
+
+            
+
+            while (rs2.next()) {
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rs2.getMetaData().getColumnCount(); i++) {
+                    //Iterate Column
+                    row.add(rs2.getString(i));
+                }
+                //System.out.println("Row [1] added " + row);
+                data1.add(row);
+
+            }
+
+            //FINALLY ADDED TO TableView
+            tablaEst1.setItems(data1);
 
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Error on Building Data");
         }
-        
-        return data1;
+       return data1;
     }
-    
-     public ObservableList<ObservableList> mostrarReporte2(TableView tablaEst1) {
-        
+
+    public ObservableList<ObservableList> mostrarReporte2(TableView tablaEst1) throws SQLException {
+
         try {
-            Connection c = db2Connection();
+
             String sql = "Select surtidor.idsurtidor,distribuidor.iddistribuidor,surtidor.tipocombustible ,round(Avg(venta.cantidadlitros),2 )as promediolitros ,round(Avg(venta.valorventa),2) as promedioventa\n"
                     + "  From  distribuidor, surtidor, venta\n"
                     + "  where distribuidor.iddistribuidor = surtidor.iddistribuidor and surtidor.idsurtidor = venta.idsurtidor \n"
                     + "  Group By surtidor.idsurtidor, distribuidor.iddistribuidor;";
 
-            ResultSet rs = c.createStatement().executeQuery(sql);
+            ResultSet rs = c2.createStatement().executeQuery(sql);
 
             for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
                 //We are using non property style for making dynamic table
@@ -209,7 +203,7 @@ public class CentralController implements Initializable {
                     //Iterate Column
                     row.add(rs.getString(i));
                 }
-                System.out.println("Row [1] added " + row);
+                //System.out.println("Row [1] added " + row);
                 data2.add(row);
 
             }
@@ -219,9 +213,9 @@ public class CentralController implements Initializable {
 
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("Error on Building Data");
+            //System.out.println("Error on Building Data");
         }
-        
+
         return data2;
     }
 
