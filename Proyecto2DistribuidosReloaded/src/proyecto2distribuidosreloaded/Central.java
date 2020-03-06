790a14df675e5b9398753e68338f7db0e7f0bc75
Nicolás H <nicolas.hervias@live.com>
2020-03-06 16:26:46 -0300

    actualización precios combustible
    se creó una base de datos de precios de combustibles para que la aplicación pueda leer los últimos cambios

---
+++ Proyecto2DistribuidosReloaded/src/proyecto2distribuidosreloaded/Central.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto2distribuidosreloaded;
+
+/**
+ *
+ * @author Nicolás Hervias
+ */
+public class Central
+{
+    private float precio93;
+    private float precio95;
+    private float precio97;
+    private float precioK;
+    private float precioD;
+    
+    public Central()
+    {
+        
+    }
+
+    /**
+     * @return the precio93
+     */
+    public float getPrecio93()
+    {
+        return precio93;
+    }
+
+    /**
+     * @param precio93 the precio93 to set
+     */
+    public void setPrecio93(float precio93)
+    {
+        this.precio93 = precio93;
+    }
+
+    /**
+     * @return the precio95
+     */
+    public float getPrecio95()
+    {
+        return precio95;
+    }
+
+    /**
+     * @param precio95 the precio95 to set
+     */
+    public void setPrecio95(float precio95)
+    {
+        this.precio95 = precio95;
+    }
+
+    /**
+     * @return the precio97
+     */
+    public float getPrecio97()
+    {
+        return precio97;
+    }
+
+    /**
+     * @param precio97 the precio97 to set
+     */
+    public void setPrecio97(float precio97)
+    {
+        this.precio97 = precio97;
+    }
+
+    /**
+     * @return the precioK
+     */
+    public float getPrecioK()
+    {
+        return precioK;
+    }
+
+    /**
+     * @param precioK the precioK to set
+     */
+    public void setPrecioK(float precioK)
+    {
+        this.precioK = precioK;
+    }
+
+    /**
+     * @return the precioD
+     */
+    public float getPrecioD()
+    {
+        return precioD;
+    }
+
+    /**
+     * @param precioD the precioD to set
+     */
+    public void setPrecioD(float precioD)
+    {
+        this.precioD = precioD;
+    }
+}
